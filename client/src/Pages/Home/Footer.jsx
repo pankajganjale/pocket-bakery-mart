f6d9a35d24a8d502be7e3c4559963b0aa135884e
ankur598 <ankursingh.as278@gmail.com>
2021-11-13 18:13:38 +0530

    done the footer part

--- client/src/Pages/Home/Footer.jsx
+++ client/src/Pages/Home/Footer.jsx
@@ -11,24 +11,58 @@ function Footer() {
             <p style={{ textAlign: "left" }}>About</p>
             <p style={{ textAlign: "left" }}>Career</p>
             <p style={{ textAlign: "left" }}>Blog</p>
-            <p style={{ textAlign: "left" }}>Terms</p>
+            <p style={{ textAlign: "left", marginTop: "100px" }}>Terms</p>
           </div>
           <div>
             <h4 style={{ marginLeft: "20px" }}>HELP AND CONTACT</h4>
-            <p style={{ textAlign: "left", marginLeft: "20px" }}>faq</p>
-            <p style={{ textAlign: "left", marginLeft: "20px" }}>About</p>
+            <p style={{ textAlign: "left", marginLeft: "20px" }}>
+              Help centers
+            </p>
+            <p style={{ textAlign: "left", marginLeft: "20px" }}>Email us</p>
+            <p
+              style={{
+                textAlign: "left",
+                marginTop: "170px",
+                marginLeft: "20px",
+              }}
+            >
+              Privacy
+            </p>
           </div>
           <div>
             <h4 style={{ marginLeft: "20px" }}>MORE FROM US</h4>
-            <p style={{ textAlign: "left", marginLeft: "20px" }}>faq</p>
-            <p style={{ textAlign: "left", marginLeft: "20px" }}>About</p>
-            <p style={{ textAlign: "left", marginLeft: "20px" }}>Career</p>
-            <p style={{ textAlign: "left", marginLeft: "20px" }}>Blog</p>
-            <p style={{ textAlign: "left", marginLeft: "20px" }}>Terms</p>
+            <p style={{ textAlign: "left", marginLeft: "20px" }}>
+              Bulk party order
+            </p>
+            <p style={{ textAlign: "left", marginLeft: "20px" }}>Cake order</p>
+            <p style={{ textAlign: "left", marginLeft: "20px" }}>
+              Make your own cake
+            </p>
+            <p style={{ textAlign: "left", marginLeft: "20px" }}>offers</p>
           </div>
         </div>
         <div id="subFooter2">
           <h3>SUBSCRIBE TO OUR NEWS LETTER</h3>
+          <div>
+            <input
+              style={{
+                outline: "none",
+                height: "25px",
+                width: "250px",
+              }}
+              placeholder="Enter your email address"
+            />
+            <button
+              style={{
+                backgroundColor: "gray",
+                height: "31px",
+                borderRadius: "0px",
+                fontSize: "small",
+              }}
+            >
+              Subscribe
+            </button>
+          </div>
         </div>
       </div>
     </>
